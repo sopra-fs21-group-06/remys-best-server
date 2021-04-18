18c3e5c69bb4563b5cd1aac25bfc2d735fc6ff91
Sandr <Sandro.Volonte@gmail.com>
2021-04-18 17:33:18 +0200

    attempted initial set up for private messaging

--- src/main/java/ch/uzh/ifi/hase/soprafs21/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/websocket/WebSocketConfig.java
@@ -14,6 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
+        //config.setUserDestinationPrefix("*");
     }
 
     @Override
