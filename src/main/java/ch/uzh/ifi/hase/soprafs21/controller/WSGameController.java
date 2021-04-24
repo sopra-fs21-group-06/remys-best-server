c21022a3f5e61bae4ac766c7ad9c3a78f0fb15a4
Pascal Emmenegger <pascalemmenegger@hotmail.ch>
2021-04-24 15:38:08 +0200

    facts websocket init

--- src/main/java/ch/uzh/ifi/hase/soprafs21/controller/WSGameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/WSGameController.java
@@ -4,9 +4,12 @@ import ch.uzh.ifi.hase.soprafs21.constant.Color;
 import ch.uzh.ifi.hase.soprafs21.objects.Game;
 import ch.uzh.ifi.hase.soprafs21.objects.GameEngine;
 import ch.uzh.ifi.hase.soprafs21.objects.Player;
+import ch.uzh.ifi.hase.soprafs21.service.GameService;
+import ch.uzh.ifi.hase.soprafs21.service.UserService;
 import ch.uzh.ifi.hase.soprafs21.service.WebSocketService;
 import ch.uzh.ifi.hase.soprafs21.utils.DogUtils;
 import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameChooseColorDTO;
+import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameReadyDTO;
 import ch.uzh.ifi.hase.soprafs21.websocket.dto.outgoing.WaitingRoomChooseColorDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,33 +17,28 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.stereotype.Controller;
 
 import java.util.List;
 import java.util.UUID;
 
 import static ch.uzh.ifi.hase.soprafs21.utils.DogUtils.getIdentity;
 
-@Controller
-public class WSChoosePlaceController {
-    Logger log = LoggerFactory.getLogger(WSChoosePlaceController.class);
+public class WSGameController {
+    Logger log = LoggerFactory.getLogger(WSGameController.class);
 
     private final GameEngine gameEngine;
     private final WebSocketService webSocketService;
 
-    public WSChoosePlaceController(GameEngine gameEngine, WebSocketService webSocketService) {
+    public WSGameController(GameEngine gameEngine, WebSocketService webSocketService) {
         this.gameEngine = gameEngine;
         this.webSocketService = webSocketService;
     }
 
-    @MessageMapping("/game/{gameId}/choose-color")
-    @SendTo("/topic/game/{gameId}/colors")
-    public synchronized WaitingRoomChooseColorDTO registerPlayer(@DestinationVariable UUID gameId, SimpMessageHeaderAccessor sha, GameChooseColorDTO gameChooseColorDTO) {
-        log.info("Player " + getIdentity(sha) + ": Choose place (color) received");
+    @MessageMapping("/game/{gameId}/ready")
+    public synchronized void ready(@DestinationVariable UUID gameId, SimpMessageHeaderAccessor sha, GameReadyDTO gameReadyDTO) {
+        log.info("Player " + getIdentity(sha) + ": Ready for game received");
         Game currentGame = gameEngine.getRunningGameByID(gameId);
-        List<Player> updatedPlayerList = currentGame.updatePlayerColor(DogUtils.convertTokenToUsername(gameChooseColorDTO.getToken(), gameEngine.getUserService()), Color.fromId(gameChooseColorDTO.getColor()));
-        log.info(updatedPlayerList.toString());
-        return DogUtils.convertPlayerListToWaitingRoomChoosecolorDTO(updatedPlayerList);
+        currentGame.setPlayerToReady(DogUtils.convertTokenToUsername(gameReadyDTO.getToken(), gameEngine.getUserService()));
     }
 }
 
