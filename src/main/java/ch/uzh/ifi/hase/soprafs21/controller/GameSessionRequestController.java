fcbbfe27fa9a072af1a9f88622ca4a0963fa8ec7
SandroVolonte <61095194+SandroVolonte@users.noreply.github.com>
2021-05-09 18:14:04 +0200

    Revert "Merge branch 'dev' of https://github.com/sopra-fs21-group-06/remys-best-server into dev"
    This reverts commit 642e25a328ee58bd381e04632232a957a3bd51b7, reversing
    changes made to 00dd363532f60829ba747e5decde8c8eaba887de.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/controller/GameSessionRequestController.java
@@ -0,0 +1,11 @@
+package ch.uzh.ifi.hase.soprafs21.controller;
+
+import ch.uzh.ifi.hase.soprafs21.objects.GameEngine;
+
+public class GameSessionRequestController {
+    private final GameEngine gameEngine;
+
+    public GameSessionRequestController(GameEngine gameEngine) {
+        this.gameEngine = gameEngine;
+    }
+}
