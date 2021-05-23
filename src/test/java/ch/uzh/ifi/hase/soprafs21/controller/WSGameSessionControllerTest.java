201c368be989f3f339a3c7a6676ede652aff71e9
Siddhant Sahu <siddhant_sahu@outlook.com>
2021-05-23 09:05:57 +0200

    fixed the plaing board assertions errors

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/WSGameSessionControllerTest.java
@@ -0,0 +1,2 @@
+package ch.uzh.ifi.hase.soprafs21.controller;public class WSGameSessionControllerTest {
+}
---
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/FriendRequestServiceTest.java
@@ -0,0 +1,2 @@
+package ch.uzh.ifi.hase.soprafs21.service;public class FriendRequestServiceTest {
+}
