670d50aab93c8898a9ad90d21001e40bf34dd020
Siddhant Sahu <siddhant_sahu@outlook.com>
2021-05-23 09:09:40 +0200

    added generateGameRequestDTO()

--- src/test/java/ch/uzh/ifi/hase/soprafs21/controller/WSGameSessionControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/controller/WSGameSessionControllerTest.java
@@ -1,2 +1,56 @@
-package ch.uzh.ifi.hase.soprafs21.controller;public class WSGameSessionControllerTest {
+package ch.uzh.ifi.hase.soprafs21.controller;
+
+import ch.uzh.ifi.hase.soprafs21.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs21.entity.User;
+import ch.uzh.ifi.hase.soprafs21.objects.Game;
+import ch.uzh.ifi.hase.soprafs21.objects.GameEngine;
+import ch.uzh.ifi.hase.soprafs21.service.CardAPIService;
+import ch.uzh.ifi.hase.soprafs21.service.UserService;
+import ch.uzh.ifi.hase.soprafs21.service.WebSocketService;
+import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameRequestAcceptDTO;
+import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameRequestDTO;
+import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameRequestDeniedDTO;
+import ch.uzh.ifi.hase.soprafs21.websocket.dto.incoming.GameSessionLeaveDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class WSGameSessionControllerTest extends AbstractWSControllerTest {
+
+    @Autowired
+    //@MockBean
+    private GameEngine gameEngine;
+
+    @MockBean
+    WebSocketService websocketService;
+
+    @MockBean
+    CardAPIService cardAPIService;
+
+    /*@MockBean
+    UserService userService;*/
+
+    private GameRequestDTO generateGameRequestDTO(String token, String username) {
+        GameRequestDTO gameRequestDTO = new GameRequestDTO();
+        gameRequestDTO.setToken(token);
+        gameRequestDTO.setUsername(username);
+        return gameRequestDTO;
+    }
+
 }
